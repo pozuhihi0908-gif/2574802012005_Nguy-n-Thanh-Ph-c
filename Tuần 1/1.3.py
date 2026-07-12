f6430dc7da3ad7e87f8218742d85c9e29d56cc0a
pozuhihi0908-gif <pozuhihi0908@gmail.com>
2026-07-12 09:49:33 +0700

    Delete Tuần 1 directory

--- Tuần 1/1.3.py
+++
@@ -1,8 +0,0 @@
-def linearsearch(arr, key):
-    for i in range(len(arr)):
-        if arr[i] == key:
-            return i 
-    return -1
-arr = ['Bao','An', 'Dat','Duc','Hung','Phi','Vinh','Dung']
-key = 'Phi'
-print("Vị trí tìm thấy thứ i là: "+str(linearsearch(arr,key)))
